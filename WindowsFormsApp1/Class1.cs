2da0856479f43b6c5359b7dcc78bb20bf7a20b59
jose luis <jluiss@bol.com.br>
2020-09-08 14:40:22 -0300

    FormWindows2

---
+++ WindowsFormsApp1/Class1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1
+{
+    class Class1
+    {
+        public int Idade { get; set; }
+        public string Name { get; set; }
+        public int CPF { get; private set; }
+
+        public void Mostrar()
+        {
+            System.Windows.Forms.MessageBox.Show("o meu nome é " + Name);
+        }
+
+    }
+}
